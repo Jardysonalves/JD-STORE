d2bb00e3d5617352e35709561604ccb97ee910da
Jardysonalves <133599630+Jardysonalves@users.noreply.github.com>
2025-12-10 12:45:21 -0300

    Add files via upload

---
+++ js/server.js
@@ -0,0 +1,12 @@
+// server.js
+
+// --- ⚠️ INFORMAÇÕES CRUCIAIS ⚠️ ---
+// COLE O SEU NOVO TOKEN NESTA LINHA:
+const BOT_TOKEN = 'MTQ0ODMzNTkwNTE5NzY1ODI2NA.GgQhtA.6QBplQYY71grDGIMPEJ3vz-o4P0MaUmRVlS_iM'; 
+const SERVER_ID = '870126035553902742'; // Lembre-se de colocar o ID do seu servidor também
+const PORT = 3000;
+// ------------------------------------
+
+
+// ... (O restante do seu código server.js) ...
+
